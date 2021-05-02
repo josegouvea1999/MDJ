753afe4a1a62f78a6a0b3c615847f1faded67b1d
josegouvea1999 <josegouvea1999@gmail.com>
2021-05-02 23:17:22 +0100

    Camera movement

---
+++ Caleb's Demons/Assets/MainCamera.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public float CameraSpeed = 5, step = 10;
+    public bool left, right, up, down, init = true;
+    public Vector3 newPos;
+
+    public static Vector3 AccelDecelInterpolation(Vector3 start, Vector3 end, float t)
+    {
+        float x = end.x - start.x;
+        float y = end.y - start.y;
+        float z = end.z - start.z;
+
+        float newT = (Mathf.Cos((t + 1) * Mathf.PI) / 2) + 0.5f;
+
+        x *= newT;
+        y *= newT;
+        z *= newT;
+
+        Vector3 retVector = new Vector3(start.x + x, start.y + y, start.z + z);
+
+        return retVector;
+    }
+
+    public void MoveLeft()
+    {
+        if (init)
+        {
+            newPos = transform.position + new Vector3(-step, 0.0f, 0.0f);
+            init = false;
+        }
+
+        float disFromCamera = Vector3.Distance(transform.position, newPos);
+
+        if (disFromCamera > 0.001f)
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * CameraSpeed);
+        else
+            left = init = false;
+    }
+
+    public void MoveRight()
+    {
+        if (init)
+        {
+            newPos = transform.position + new Vector3(step, 0.0f, 0.0f);
+            init = false;
+        }
+
+        float disFromCamera = Vector3.Distance(transform.position, newPos);
+
+        if (disFromCamera > 0.001f)
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * CameraSpeed);
+        else
+            right = init = false;
+    }
+
+    public void MoveUp()
+    {
+        if (init)
+        {
+            newPos = transform.position + new Vector3(0.0f, step, 0.0f);
+            init = false;
+        }
+
+        float disFromCamera = Vector3.Distance(transform.position, newPos);
+
+        if (disFromCamera > 0.001f)
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * CameraSpeed);
+        else
+            up = init = false;
+    }
+
+    public void MoveDown()
+    {
+        if (init)
+        {
+            newPos = transform.position + new Vector3(0.0f, -step, 0.0f);
+            init = false;
+        }
+
+        float disFromCamera = Vector3.Distance(transform.position, newPos);
+
+        if (disFromCamera > 0.001f)
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * CameraSpeed);
+        else
+            down = init = false;
+    }
+
+
+    void Update()
+    {
+        if (left)
+            MoveLeft();
+
+        else if (right)
+            MoveRight();
+
+        else if (up)
+            MoveUp();
+
+        else if (down)
+            MoveDown();
+    }
+
+    void Start()
+    {
+
+    }
+}
