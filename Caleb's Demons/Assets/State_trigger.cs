f26c9ab22a57989d204377cbc58202ad459b75e4
Rafael Henriques <rafael.henriques@tecnico.ulisboa.pt>
2021-05-03 23:09:16 +0100

    Trigger decides speed

--- Caleb's Demons/Assets/State_trigger.cs
+++ Caleb's Demons/Assets/State_trigger.cs
@@ -7,25 +7,31 @@ public class State_trigger : MonoBehaviour
     Collider2D _collider;
     public int wait = 1;
     public int state = 2;
+    public int side_vel = 5;
+
+    //if state is 2
+    public int jump_force = 4;
+
+
+    //if state is 3 (climb)
     public bool down = false;
-    public float distance = 10;
+    public int climb_distance = 10;
+
+
     void Start()
     {
         _collider = GetComponent<Collider2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Caleb")
         {
             Caleb caleb = collision.gameObject.GetComponent<Caleb>();
-            caleb.State_change(wait, state,down,distance);
+            caleb.State_change(wait, state, side_vel, jump_force, down, climb_distance);
         }
     }
 }
