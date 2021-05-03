1df99ecf5eb6d7d98b486d1f8299b1e9ad189f76
Rafael Henriques <rafael.henriques@tecnico.ulisboa.pt>
2021-05-04 00:16:02 +0100

    IsGrounded Change

--- Caleb's Demons/Assets/Caleb.cs
+++ Caleb's Demons/Assets/Caleb.cs
@@ -127,7 +127,7 @@ public class Caleb : MonoBehaviour
 
     private bool isGrouned()
     {
-        float extrahieght = .01f;
+        float extrahieght = .1f;
         RaycastHit2D raycasthit = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.bounds.size, 0f, Vector2.down, extrahieght);
         /*Color raycolor;
         if(raycasthit.collider != null)
