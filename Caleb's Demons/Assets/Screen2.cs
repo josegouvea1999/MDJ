25afc33ec7e7668ff77c530d47edc23a418ef8db
IST180873 <pedro.silva.freire@tecnico.ulisboa.pt>
2021-05-04 02:05:19 +0100

    added stuff

---
+++ Caleb's Demons/Assets/Screen2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Caleb")
+        {
+
+            Debug.Log("next Screen");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Caleb's Demons/Assets/screen1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Caleb")
+        {
+            Camera.main.GetComponent<MainCamera>().right=true;
+            Debug.Log("next Screen");
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
